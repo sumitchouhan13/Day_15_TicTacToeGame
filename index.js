90b35c3eeefe918142725a7b67165ec8356ee054
Sumit Chouhan <sumitchouhan8933@gmail.com>
2021-09-22 22:37:56 +0530

    first Game

---
+++ index.js
@@ -0,0 +1,37 @@
+// function circle() {
+//   console.log("circle");
+// }
+
+// function cross() {
+//   console.log("cross");
+// }
+// var numberOfMoves = 9;
+// var turn = true;
+
+// var fun = function () {
+//   if (turn) {
+//     document.getElementById("circlePlayer").disabled = true;
+//     document.getElementById("crossPlayer").onclick = cross;
+//   } else {
+//     document.getElementById("crossPlayer").disabled = true;
+//     document.getElementById("circlePlayer").onclick = circle;
+//   }
+//   turn = !!turn;
+// };
+// document.getElementById("startGame").onclick = start;
+// function start() {
+//   for (let i = 1; i <= numberOfMoves; i++) {
+//     console.log(i);
+//     fun;
+//   }
+// }
+
+// const cellElements = document.querySelectorAll("[data-cell]");
+
+// cellElements.forEach((cell) => {
+//   cell.addEventListener("click", handleClick, { once: true });
+// });
+
+// function handleClick(e) {
+//   console.log("clicked");
+// }
